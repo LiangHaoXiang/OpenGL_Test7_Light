9c2a042fb0b617f3dc7a1890438228f158ffbb90
LiangHaoXiang <1013568397@qq.com>
2021-07-16 17:20:10 +0800

    在观察空间（而不是世界空间）中计算冯氏光照

--- OpenGL_Test7_Light/LightShader/LightVertexShader.cpp
+++ OpenGL_Test7_Light/LightShader/LightVertexShader.cpp
@@ -6,14 +6,18 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-out vec3 FragPos;   //片段的世界空间坐标位置
+uniform vec3 lightPos;
+
+out vec3 FragPos;   //片段的坐标位置
 out vec3 Normal;    //片段的法向量
+out vec3 LightPos;  //光源的坐标
 
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    FragPos = vec3(model * vec4(aPos, 1.0));
+    FragPos = vec3(view * model * vec4(aPos, 1.0));
     //使用inverse和transpose函数自己生成这个法线矩阵
     //法线矩阵让法向量转换在世界空间坐标中
-    Normal = mat3(transpose(inverse(model))) * aNormal;
+    Normal = mat3(transpose(inverse(view * model))) * aNormal;
+    LightPos = vec3(view * vec4(lightPos, 1.0));
 }
